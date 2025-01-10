3e50e6cc5e05f6899b177f5f0915b1dc36d54ce0
Oceane86 <124265385+Oceane86@users.noreply.github.com>
2025-01-10 13:41:30 +0100

    Update profil page.jsx

--- app/profil/page.jsx
+++ app/profil/page.jsx
@@ -1,17 +1,18 @@
 // app/profil/page.jsx
-'use client';
 
+"use client";
+
+import Header from "@/components/Header";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-import Header from "@/components/Header";
+import { useEffect, useState } from "react";
+import EditProfileForm from "@/components/EditProfileForm";
+
 export default function ProfilePage() {
   const { data: session, status } = useSession();
   const router = useRouter();
   const [formData, setFormData] = useState({
     username: "",
-    firstname: "",
-    lastname: "",
     email: "",
     address: {
       street: "",
@@ -21,12 +22,44 @@ export default function ProfilePage() {
       postalCode: "",
     },
   });
+  const [activeTab, setActiveTab] = useState("information");
 
-  if (status === "loading") {
-    return <p>Chargement...</p>;
-  }
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    }
+  }, [status, router]);
 
-  if (!session) {
+  useEffect(() => {
+    if (session?.user) {
+      const fetchUserData = async () => {
+        try {
+          const response = await fetch(`/api/user/${session.user.id}`);
+          if (!response.ok) throw new Error("Erreur lors de la récupération des données.");
+          const data = await response.json();
+          setFormData({
+            username: data.username || "",
+            email: data.email || "",
+            address: {
+              street: data.address?.street || "",
+              city: data.address?.city || "",
+              state: data.address?.state || "",
+              country: data.address?.country || "",
+              postalCode: data.address?.postalCode || "",
+            },
+          });
+        } catch (error) {
+          console.error("Erreur lors du chargement des données utilisateur:", error);
+        }
+      };
+
+      fetchUserData();
+    }
+  }, [session]);
+
+  if (status === "loading") return <p className="text-center mt-10">Chargement...</p>;
+
+  if (!session)
     return (
       <div className="container mx-auto p-4">
         <p>Veuillez vous connecter pour voir votre profil.</p>
@@ -38,13 +71,10 @@ export default function ProfilePage() {
         </button>
       </div>
     );
-  }
-
-  const { user } = session;
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
-    if (name.includes("address.")) {
+    if (name.startsWith("address.")) {
       const key = name.split(".")[1];
       setFormData((prev) => ({
         ...prev,
@@ -58,7 +88,7 @@ export default function ProfilePage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("/api/updateProfile", {
+      const response = await fetch("/api/profil/update", {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
@@ -71,140 +101,63 @@ export default function ProfilePage() {
     }
   };
 
+  const handleSignOut = async () => {
+    try {
+      await signOut({ callbackUrl: "/" });
+    } catch (error) {
+      console.error("Erreur lors de la déconnexion :", error);
+    }
+  };
+
   return (
-      <>
-        <Header page={"mon-compte"} />
-        <div className="container mx-auto p-4">
-          <h1 className="text-2xl font-bold mb-6">Mon Profil</h1>
-          <div className="flex items-center mb-6">
-            <img
-                src={user.image || "/assets/user_default.svg"}
-                alt={user.username || "Utilisateur"}
-                className="w-20 h-20 object-cover rounded-full mr-6"
-            />
-            <div>
-              <h2 className="text-xl font-semibold">{user.username || "Nom de l'utilisateur"}</h2>
-              <p className="text-gray-600 text-sm">{user.email || "Email non disponible"}</p>
-            </div>
-          </div>
+    <><Header /><div className="container mx-auto p-8">
+      <h1 className="text-2xl font-bold mb-6">Mon Profil</h1>
 
-          <form onSubmit={handleSubmit} className="bg-white shadow-sm rounded p-6">
-            <h2 className="text-xl font-semibold mb-4">Informations générales</h2>
-            <div className="mb-4">
-              <label className="block mb-1">Nom d'utilisateur</label>
-              <input
-                  type="text"
-                  name="username"
-                  value={formData.username}
-                  onChange={handleInputChange}
-                  placeholder={user.username || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Prénom</label>
-              <input
-                  type="text"
-                  name="firstname"
-                  value={formData.firstname}
-                  onChange={handleInputChange}
-                  placeholder={user.firstname || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Nom</label>
-              <input
-                  type="text"
-                  name="lastname"
-                  value={formData.lastname}
-                  onChange={handleInputChange}
-                  placeholder={user.lastname || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Email</label>
-              <input
-                  type="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                  placeholder={user.email || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-
-            <h2 className="text-xl font-semibold mb-4">Adresse</h2>
-            <div className="mb-4">
-              <label className="block mb-1">Rue</label>
-              <input
-                  type="text"
-                  name="address.street"
-                  value={formData.address.street}
-                  onChange={handleInputChange}
-                  placeholder={user.address?.street || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Ville</label>
-              <input
-                  type="text"
-                  name="address.city"
-                  value={formData.address.city}
-                  onChange={handleInputChange}
-                  placeholder={user.address?.city || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">État</label>
-              <input
-                  type="text"
-                  name="address.state"
-                  value={formData.address.state}
-                  onChange={handleInputChange}
-                  placeholder={user.address?.state || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Pays</label>
-              <input
-                  type="text"
-                  name="address.country"
-                  value={formData.address.country}
-                  onChange={handleInputChange}
-                  placeholder={user.address?.country || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block mb-1">Code postal</label>
-              <input
-                  type="text"
-                  name="address.postalCode"
-                  value={formData.address.postalCode}
-                  onChange={handleInputChange}
-                  placeholder={user.address?.postalCode || ""}
-                  className="w-full border rounded p-2"
-              />
-            </div>
-            <button
-                type="submit"
-                className="mt-4 px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            >
-              Sauvegarder
-            </button>
-          </form>
-          <button
-              onClick={() => signOut()}
-              className="mt-4 px-6 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+      <div className="tabs">
+        <ul className="flex space-x-4 border-b">
+          <li
+            className={`tab cursor-pointer ${activeTab === "information" ? "border-b-2 border-black" : ""}`}
+            onClick={() => setActiveTab("information")}
           >
-            Se déconnecter
-          </button>
+            Modifier mes informations
+          </li>
+          <li
+            className={`tab cursor-pointer ${activeTab === "profil" ? "border-b-2 border-black" : ""}`}
+            onClick={() => setActiveTab("profil")}
+          >
+            Profil
+          </li>
+        </ul>
+
+        <div className="tab-content mt-6">
+          {activeTab === "information" && (
+            <EditProfileForm
+              formData={formData}
+              handleInputChange={handleInputChange}
+              handleSubmit={handleSubmit} />
+          )}
+          {activeTab === "profil" && (
+            <div>
+              <h2 className="text-xl font-semibold mb-4">Mes informations :</h2>
+              <p><strong>Nom d'utilisateur:</strong> {formData.username}</p>
+              <p><strong>Email:</strong> {formData.email}</p>
+              <h3 className="mt-4 font-semibold">Adresse :</h3>
+              {Object.entries(formData.address).map(([key, value]) => (
+                <p key={key}>
+                  <strong>{key}:</strong> {value}
+                </p>
+              ))}
+            </div>
+          )}
         </div>
-      </>
+      </div>
+
+      <button
+        onClick={handleSignOut}
+        className="mt-4 px-6 py-2 bg-red-500 text-white rounded hover:bg-red-600"
+      >
+        Se déconnecter
+      </button>
+    </div></>
   );
 }
